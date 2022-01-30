79d419b4a07956d78345b0afbb90c75ef20c5e0d
João Reis <jype_13@hotmail.com>
2022-01-30 16:36:29 +0000

    Add Settings

--- Assets/Scripts/SoundOffInput.cs
+++ Assets/Scripts/SoundOffInput.cs
@@ -6,17 +6,6 @@ using TMPro;
 public class SoundOffInput : MonoBehaviour
 {
 
-    [SerializeField] TextMeshProUGUI soundOFF;
-
-    Color inactiveColor = new Color(0, 0, 0, 1f);
-    Color activeColor = new Color(237/255f, 108/255f, 97/255f, 1f);
-
-    // Update is called once per frame
-    void Update()
-    {
-          soundOFF.color = 0 != AudioListener.volume ? inactiveColor : activeColor;
-    }
-
     public void soundOffCallback()
     {
         AudioListener.volume = 0;
--- Assets/Scripts/SoundOnInput.cs
+++ Assets/Scripts/SoundOnInput.cs
@@ -5,15 +5,12 @@ using TMPro;
 public class SoundOnInput : MonoBehaviour
 {
 
-    [SerializeField] TextMeshProUGUI soundON;
-
     Color inactiveColor = new Color(0, 0, 0, 1f);
     Color activeColor = new Color(94/255f, 156/255f, 81/255f, 1f);
 
     // Update is called once per frame
     void Update()
     {
-        soundON.color = 0 == AudioListener.volume ? inactiveColor : activeColor;
     }
 
     public void soundOnCallback()
